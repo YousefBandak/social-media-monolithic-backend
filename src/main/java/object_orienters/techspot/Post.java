6f30d6738f6a6daa9e7309d7a2385f9a7cf42048
Angela Salem <angel.salem.008@gmail.com>
2024-03-05 11:12:54 +0200

    Added Comment, Reaction, and Post classes. Relations need to be modified

---
+++ src/main/java/object_orienters/techspot/Post.java
@@ -0,0 +1,26 @@
+package object_orienters.techspot;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import lombok.Data;
+
+import java.sql.Timestamp;
+
+@Entity
+@Data
+public class Post implements Content{
+    @Id
+    private String postId;
+
+    @OneToOne
+    private User author;
+    private Timestamp timestamp;
+    private String content;
+    private Privacy privacy;
+    private int numOfComments;
+    private int numOfLikes;
+
+
+}

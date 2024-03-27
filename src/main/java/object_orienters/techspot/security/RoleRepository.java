c44527937b4830c168941cbe19bf824217f6abb6
YousefBandak <yousef.bandak@gmail.com>
2024-03-27 18:46:21 +0200

    FINISHED SECURITY WITH JET :DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD

---
+++ src/main/java/object_orienters/techspot/security/RoleRepository.java
@@ -0,0 +1,11 @@
+package object_orienters.techspot.security;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(ERole name);
+}

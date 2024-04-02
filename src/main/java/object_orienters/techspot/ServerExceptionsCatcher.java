0164397351d2bbb20909222dde1a1de14360a982
Angela Salem <angel.salem.008@gmail.com>
2024-04-02 20:19:31 +0300

    added a server error global catcher (ControllerAdvice)

---
+++ src/main/java/object_orienters/techspot/ServerExceptionsCatcher.java
@@ -0,0 +1,42 @@
+package object_orienters.techspot;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.time.LocalDateTime;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@ControllerAdvice(basePackages = {
+        //"object_orienters.techspot.message",
+        "object_orienters.techspot.comment",
+        "object_orienters.techspot.profile",
+        "object_orienters.techspot.security",
+        "object_orienters.techspot.chat",
+        "object_orienters.techspot.content",
+        "object_orienters.techspot.feed",
+        "object_orienters.techspot.model",
+        "object_orienters.techspot.post",
+        "object_orienters.techspot.reaction"
+})
+public class ServerExceptionsCatcher extends ResponseEntityExceptionHandler {
+    @ExceptionHandler({Exception.class})
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ResponseBody
+    public ResponseEntity handleAuthenticationException(Exception ex) {
+
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
+        body.put("errors", ex.getMessage());
+
+        return new ResponseEntity<>(body, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+
+}

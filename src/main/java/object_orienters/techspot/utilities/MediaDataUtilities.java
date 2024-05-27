2cad884f3e606758e240075529c33e3fb6877ca5
Yousef Albandak <yousef.bandak@gmail.com>
2024-05-27 12:41:34 +0300

    made changes to files, removed code redundancy

--- src/main/java/object_orienters/techspot/utilities/MediaDataUtilities.java
+++ src/main/java/object_orienters/techspot/utilities/MediaDataUtilities.java
@@ -8,6 +8,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.google.api.client.util.Data;
+
 import java.util.List;
 
 @Service
@@ -15,11 +17,26 @@ public class MediaDataUtilities {
 
     FileStorageService fileStorageService;
     DataTypeRepository dataTypeRepository;
+
     @Autowired
     public MediaDataUtilities(FileStorageService fileStorageService, DataTypeRepository dataTypeRepository) {
         this.fileStorageService = fileStorageService;
         this.dataTypeRepository = dataTypeRepository;
     }
+
+    public DataType handleAddFile(MultipartFile file) {
+        DataType pic = new DataType();
+        String fileName = fileStorageService.storeFile(file);
+        String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path("/media_uploads/")
+                .path(fileName)
+                .toUriString();
+        pic.setType(file.getContentType());
+        pic.setFileName(fileName);
+        pic.setFileUrl(fileDownloadUri);
+        return pic;
+    }
+
     public void handleAddMediaData(List<MultipartFile> files, List<DataType> allMedia) {
         files.stream().forEach((file) -> {
             DataType media = new DataType();

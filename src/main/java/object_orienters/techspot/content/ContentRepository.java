41d43244649a2dc2222bdb1280a6285cdf469e7d
Angela Salem <angel.salem.008@gmail.com>
2024-06-01 09:12:40 +0300

    fixed a bug in followers/following

--- src/main/java/object_orienters/techspot/content/ContentRepository.java
+++ src/main/java/object_orienters/techspot/content/ContentRepository.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 @Repository
 public interface ContentRepository extends PagingAndSortingRepository<Content, Long> {
@@ -24,7 +25,7 @@ public interface ContentRepository extends PagingAndSortingRepository<Content, L
     Page<Content> findAllByMainAuthorAndContentTypeAndPrivacy(@Param("author") Profile author, @Param("privacies") List<Privacy> privacies, @Param("contentTypes") List<ContentType> contentTypes, Pageable pageable);
 
     @Query("SELECT c FROM Content c WHERE c.contentAuthor IN :authors AND c.privacy IN :privacies AND c.contentType IN :contentTypes")
-    Page<Content> findAllByMainAuthorsAndContentTypeAndPrivacy(@Param("authors") List<Profile> authors, @Param("privacies") List<Privacy> privacies, @Param("contentTypes") List<ContentType> contentTypes, Pageable pageable);
+    Page<Content> findAllByMainAuthorsAndContentTypeAndPrivacy(@Param("authors") Set<Profile> authors, @Param("privacies") List<Privacy> privacies, @Param("contentTypes") List<ContentType> contentTypes, Pageable pageable);
 
 
 }
